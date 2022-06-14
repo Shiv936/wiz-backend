00dc69f26c80e898cd92c3d6d5d62f0a82094aa8
Rohan Gupta <rohan.gupta@wizfreight.com>
2022-06-14 11:00:56 +0530

    Read and delete handlers

--- internal/handlers/api/v1/ports/delete.go
+++ internal/handlers/api/v1/ports/delete.go
@@ -7,6 +7,31 @@ import (
 )
 
 func (h *Handler) Delete(ctx *gin.Context) {
+	var query = ctx.Param("id")
+	if err := h.portsService.Remove(query); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-	ctx.JSON(http.StatusOK, "Hi There")
+	ctx.JSON(http.StatusOK, "Successfully deleted")
+	// deleteRequest
+	// if err := ctx.ShouldBindQuery(&query); err != nil {
+	// 	ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	// var isoCode string = ""
+	// if query.IsoCode != nil {
+	// 	isoCode = *query.IsoCode
+	// }
+
+	// err := h.portsService.Remove(isoCode)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	// 	return
+	// }
+	// ctx.JSON(http.StatusOK, gin.H{})
 }
+
+// type deleteRequest struct {
+// 	IsoCode *string `form:"iso" json:"iso" binding:"required,alpha"`
+// }
--- internal/handlers/api/v1/ports/read.go
+++ internal/handlers/api/v1/ports/read.go
@@ -7,6 +7,31 @@ import (
 )
 
 func (h *Handler) Read(ctx *gin.Context) {
+	var query readRequest
+	if err := ctx.ShouldBindJSON(&query); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
 
-	ctx.JSON(http.StatusOK, "Hi There")
+	var isoCode string = ""
+	if query.PORT_ISO_CODE != nil {
+		isoCode = *query.PORT_ISO_CODE
+	}
+
+	ports, found, err := h.portsService.Fetch(isoCode)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !found {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "ports not found"})
+	} else {
+		ctx.JSON(http.StatusOK, ports)
+	}
+}
+
+type readRequest struct {
+	PORT_ISO_CODE *string `form:"piso" json:"iso" binding:"required"`
 }

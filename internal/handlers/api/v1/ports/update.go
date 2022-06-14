f38b9dfce8b6cd1933c20b77f7fe58df3fdfa680
rethenyaBalakumar <rethenyabalakumar@gmail.com>
2022-06-14 17:42:29 +0530

    ports update handlers

--- internal/handlers/api/v1/ports/update.go
+++ internal/handlers/api/v1/ports/update.go
@@ -7,6 +7,33 @@ import (
 )
 
 func (h *Handler) Update(ctx *gin.Context) {
+	var query updateRequest
+	if err := ctx.ShouldBindJSON(&query); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		return
+	}
+	port, err := h.portsService.Modify(
+		query.Code,
+		query.Name,
+		query.Type,
+		query.CountryIsoCode,
+		query.Latitude,
+		query.Longitude,
+		query.IsActive,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, port)
+}
 
-	ctx.JSON(http.StatusOK, "Hi There")
+type updateRequest struct {
+	Code           string   `json:"iso" binding:"required"`
+	Name           *string  `json:"nm" `
+	Type           *string  `json:"ty" `
+	CountryIsoCode *string  `json:"ciso" `
+	Latitude       *float64 `json:"lat" `
+	Longitude      *float64 `json:"lon" `
+	IsActive       *bool    `json:"ia" `
 }
